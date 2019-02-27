228c93b0948543e737206b7cc8abba0e8ceb971f
Sergio Uve <sergiouve@gmail.com>
2019-02-27 17:58:56 +0100

    Udpate build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,41 @@
-const gulp        = require('gulp');
-const gutil       = require('gulp-util');
-const uglify      = require('gulp-uglify');
-const browserify  = require('browserify');
-const sass        = require('gulp-sass');
-const source      = require('vinyl-source-stream');
-const shell       = require('gulp-shell');
-const babelify    = require('babelify');
+const gulp       = require('gulp')
+const gutil      = require('gulp-util')
+const uglify     = require('gulp-uglify')
+const browserify = require('browserify')
+const sass       = require('gulp-sass')
+const source     = require('vinyl-source-stream')
+const shell      = require('gulp-shell')
+const babelify   = require('babelify')
 
-gulp.task('browserify', function() {
+gulp.task('browserify', () => {
     return browserify('./src/js/app.js')
         .transform(babelify, {presets: ["es2015"]})
         .bundle()
         .pipe(source('app.js'))
-        .pipe(gulp.dest('./assets/js/'));
-});
+        .pipe(gulp.dest('./dist/assets/js/'))
+})
 
-gulp.task('sass', function() {
+gulp.task('sass', done => {
     gulp.src('./src/scss/app.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./assets/css'));
-});
+        .pipe(gulp.dest('./dist/assets/css'))
+    done()
+})
+
+gulp.task('cp', shell.task([
+    'cp index.html ./dist/index.html',
+    'cp favicon.ico ./dist/favicon.ico'
+]))
 
 gulp.task('serve', shell.task([
-  'http-server ./ -p 3000'
-]));
+  'http-server ./dist -p 3000'
+]))
 
-gulp.task('watch', function() {
-    gulp.watch('./src/js/*/*.js', ['browserify']);
-    gulp.watch('./src/js/*.js', ['browserify']);
-    gulp.watch('./src/scss/*/*.scss', ['sass']);
-    gulp.watch('./src/scss/*.scss', ['sass']);
-});
+gulp.task('watch', () => {
+  gulp.watch('./src/js/*/*.js', gulp.series('browserify'))
+    gulp.watch('./src/js/*.js', gulp.series('browserify'))
+    gulp.watch('./src/scss/*/*.scss', gulp.series('sass'))
+    gulp.watch('./src/scss/*.scss', gulp.series('sass'))
+})
 
-gulp.task('default', ['browserify', 'sass', 'serve', 'watch']);
+gulp.task('default', gulp.series(['browserify', 'sass', 'cp', 'serve', 'watch']))
--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,6 @@ const repository = require('./lib/repository');
 const utils      = require('./lib/utils.js');
 const stats      = require('./pages/stats.js');
 
-config.token == undefined ? config.token = utils.getHashParameter('access_token') : config.token = config.token;
+config.token === undefined ? config.token = utils.getHashParameter('access_token') : config.token = config.token;
+
 stats.init();
